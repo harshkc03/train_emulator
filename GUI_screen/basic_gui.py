ded4df39cfa6e5b9569f6db2c29d13cf3909b463
Gokul-0hno <gokul6102011@gmail.com>
2021-08-02 14:02:07 +0530

    Function added

--- GUI_screen/basic_gui.py
+++ GUI_screen/basic_gui.py
@@ -7,15 +7,22 @@ my_Var = 0
 MainWindow = Tk()
 MainWindow.geometry('500x500')
 
+
+#Functions for use
+def changeValue(x):
+    x+=1
+    MainWindow.update
+    
+
+
 #Button
 button = Button(MainWindow, text ='DIE!',background='black',foreground='white',
-                activebackground='black',activeforeground='white', command=MainWindow.destroy)  
+                activebackground='black',activeforeground='white', command=changeValue(my_Var))  
 button.place(x=10,y=10,height=50,width=100) 
 
-#Button Command
-def changeValue():
-    global my_Var
-    my_Var+=1
+#Label
+label=Label(MainWindow,text=my_Var,borderwidth=5,)
+label.place(x=10,y=80,height=50,width=100)
 
   
 # Tkinter event loop
