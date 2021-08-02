724e01df937cb73a42613f0c5a1467d133586eaf
harshkc03 <harshkc2000@gmail.com>
2021-08-02 14:15:08 +0530

    button increment done

--- GUI_screen/basic_gui.py
+++ GUI_screen/basic_gui.py
@@ -6,24 +6,24 @@ my_Var = 0
 #MainWindow
 MainWindow = Tk()
 MainWindow.geometry('500x500')
-
+    
+#Label
+label=Label(MainWindow,text=str(my_Var),borderwidth=5,)
+label.place(x=10,y=80,height=50,width=100)
 
 #Functions for use
-def changeValue(x):
-    x+=1
-    MainWindow.update
-    
+def changeValue():
 
+    global my_Var
+    my_Var+=1
+
+    #Update text
+    label.configure(text = str(my_Var))
 
 #Button
 button = Button(MainWindow, text ='DIE!',background='black',foreground='white',
-                activebackground='black',activeforeground='white', command=changeValue(my_Var))  
+                activebackground='black',activeforeground='white', command=changeValue)  
 button.place(x=10,y=10,height=50,width=100) 
 
-#Label
-label=Label(MainWindow,text=my_Var,borderwidth=5,)
-label.place(x=10,y=80,height=50,width=100)
-
-  
 # Tkinter event loop
 MainWindow.mainloop()   
