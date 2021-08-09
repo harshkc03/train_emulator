e55f5ca7146306d63c87c724d02af6fa5dde7807
Gokul-0hno <gokul6102011@gmail.com>
2021-08-09 21:15:19 +0530

    hello

--- GUI_screen/MenuWithButtons.py
+++ GUI_screen/MenuWithButtons.py
@@ -13,6 +13,7 @@ class TheContainerClass:
         self.mainFrame.configure(background='black')
         self.menu1=Frame(self.mainFrame)
         self.menu2=Frame(self.mainFrame)
+        self.menu3=Frame(self.mainFrame)
         
         #Frame1_Widgets
         self.label1=Label(self.menu1)
@@ -22,11 +23,18 @@ class TheContainerClass:
         #Frame2_Widgets
         self.label2=Label(self.menu2)
         self.label2.configure(text='I am from menu 2',justify=LEFT,background='green',foreground='white')
-        self.label2.place(x=10,y=110,height=40,width=300)
+        self.label2.place(x=10,y=70,height=40,width=300)
+
+        #Frame3_Widgets
+        self.label3=Label(self.menu3)
+        self.label3.configure(text='I am Menu3 da launda',justify=RIGHT,background='blue',foreground='white')
+        self.label3.place(x=10,y=110,height=40,width=300)
+
 
         #Frame_Control_Buttons
         self.menuButton1=Radiobutton(text='Menu 1',variable=9999,value=1,indicatoron=False,command=self.menuButton1Clicked)
         self.menuButton2=Radiobutton(text='Menu 2',variable=9999,value=2,indicatoron=False,command=self.menuButton2Clicked)
+        self.menuButton3=Radiobutton(text='Menu 3',variable=9999,value=3,indicatoron=False,command=self.menuButton3Clicked)
         self.menuButton1.select()
 
         #Packing
@@ -34,21 +42,25 @@ class TheContainerClass:
         self.menu1.pack(fill=BOTH,expand=True)
         self.menuButton1.pack(side=LEFT,fill=X,expand=True,ipady=5)
         self.menuButton2.pack(side=LEFT,fill=X,expand=True,ipady=5)
+        self.menuButton3.pack(side=LEFT,fill=X,expand=True,ipady=5)
 
      #Functions
     def menuButton1Clicked(self):
          self.menu1.pack(fill=BOTH,expand=True)
          self.menu2.pack_forget()
+         self.menu3.pack_forget()
 
     def menuButton2Clicked(self):
          self.menu1.pack_forget()
          self.menu2.pack(fill=BOTH,expand=True)
+         self.menu3.pack_forget()
+
+    def menuButton3Clicked(self):
+         self.menu1.pack_forget()
+         self.menu2.pack_forget()
+         self.menu3.pack(fill=BOTH,expand=True)
 
         
 
 gui=TheContainerClass()
-gui.MainWindow.mainloop()
-        
-        
-
-        
+gui.MainWindow.mainloop()     
