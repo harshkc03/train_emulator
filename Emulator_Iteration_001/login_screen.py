b2ccec0b07d4a97e08f1bb2ff467c0ea08c69726
Gokul-0hno <gokul6102011@gmail.com>
2022-01-03 12:22:03 +0530

    classes trials

--- Emulator_Iteration_001/login_screen.py
+++ Emulator_Iteration_001/login_screen.py
@@ -1,10 +1,12 @@
 from tkinter import *
-from types import CellType
 
-class Login(Frame):
+class Login:
+
     #Initializer
-    def __init__(self,window):
-        Frame.__init__(self,window)
-
-        self.config(bg="red")
+    def __init__(self,master):
+        self.fancyFrame=Frame(master,bg="white",height=500,width=800)
+        self.userFrame=Frame(self.fancyFrame,bg="#000000",height=490,width=790).grid(in_=self.fancyFrame)
+        self.userLabel=Label(self.userFrame,text="User",bg="#000000",font="15",fg="White").grid(in_=self.userFrame)
+        
+        
         
