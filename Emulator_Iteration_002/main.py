af1940e8f55c1808788256d6f809f1ca0281423c
Gokul-0hno <gokul6102011@gmail.com>
2022-01-04 10:59:16 +0530

    updates 01

--- Emulator_Iteration_002/main.py
+++ Emulator_Iteration_002/main.py
@@ -36,6 +36,20 @@ class MainWindow(Tk):
         #Parent frame for all sub frames
         self.topLevelFrame=Frame(self,bg="white")
 
+        #Sub Frames
+        self.frame01=Frame(master=self.topLevelFrame)
+        self.frame02=Frame(master=self.topLevelFrame)
+        self.frame03=Frame(master=self.topLevelFrame)
+        self.frame04=Frame(master=self.topLevelFrame)
+        self.frame05=Frame(master=self.topLevelFrame)
+        self.frame06=Frame(master=self.topLevelFrame)
+        self.frame07=Frame(master=self.topLevelFrame)
+        self.frame08=Frame(master=self.topLevelFrame)
+        self.frame09=Frame(master=self.topLevelFrame)
+        self.frame10=Frame(master=self.topLevelFrame)
+        self.frame11=Frame(master=self.topLevelFrame)
+
+        
         #Dictionary containing buttons and an index
         self.buttonDict={self.button01:1,
                          self.button02:2,
@@ -49,17 +63,42 @@ class MainWindow(Tk):
                          self.button10:10,
                          self.button11:11}
 
+        #Dictionary containing frames and an index
+        self.frameDict={self.frame01:1,
+                        self.frame02:2,
+                        self.frame03:3,
+                        self.frame04:4,
+                        self.frame05:5,
+                        self.frame06:6,
+                        self.frame07:7,
+                        self.frame08:8,
+                        self.frame09:9,
+                        self.frame10:10,
+                        self.frame11:11,}
+
     def configureWidgets(self):
+        #info Frame grid configuration
         self.gridConfigure(1,self.numberOfColumns,self.infoFrame)
 
+        #configuring buttons
         for button,index in self.buttonDict.items():
-            button.config(text="Button "+str(index),value=index,indicatoron=0,variable=self.mainRadioButtonVar,relief='flat',bg="#FFFFFF")
-                
+            button.config(text="Button "+str(index),value=index,indicatoron=0,
+                          variable=self.mainRadioButtonVar,relief='flat',
+                          bg="#FFFFFF",offrelief='flat',command=self.buttonFunc(index))
+
+        
+
+        #configuring topLevelFrame grid       
         self.gridConfigure(self.numberOfRows-2,self.numberOfColumns,self.topLevelFrame)
+
+        #configuring sub frames of topLevelFrame
+        for frame in self.frameDict:
+            frame.config(bg="#000000")
+            self.gridConfigure(self.numberOfRows-2,self.numberOfColumns,frame)
         
     def placeWidgets(self):
         #infoFrame
-        self.infoFrame.grid(row=0,column=0,columnspan=self.numberOfColumns,padx=2,sticky="news",pady=2)
+        self.infoFrame.grid(row=0,column=0,columnspan=self.numberOfColumns,sticky="news",pady=2)
 
         #info Frame Children
         self.trainNoFrame.grid(row=0,column=0,columnspan=2,padx=2,pady=2,sticky="news")
@@ -72,7 +111,9 @@ class MainWindow(Tk):
             button.grid(row=1,column=index-1,sticky="news",padx=2)
 
         #TopLevelFrame
-        self.topLevelFrame.grid(row=2,rowspan=self.numberOfRows-2,column=0,columnspan=self.numberOfColumns,sticky="news",padx=2,pady=2)
+        master=self.topLevelFrame.grid(row=2,rowspan=self.numberOfRows-2,
+                                       column=0,columnspan=self.numberOfColumns,
+                                       sticky="news",pady=2)
 
     def gridConfigure(self,rows,columns,root):
         root.update()
@@ -81,6 +122,18 @@ class MainWindow(Tk):
         for x in range(columns):
             root.columnconfigure(x,weight=1)
     
+    def displayFrame(self,frameIndex):
+        for frame,index in self.frameDict.items():
+            if index==frameIndex:
+                frame.grid(rowspan=self.numberOfRows-2,columnspan=self.numberOfColumns,
+                           padx=2,pady=2,sticky="news")
+            else:
+                frame.grid_remove()
+
+    def buttonFunc(self,index):
+        print(str(index))
+        self.displayFrame(index)
+
     #Variables
     numberOfRows=12
     numberOfColumns=11
